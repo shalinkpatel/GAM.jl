b6a12519304950646723464477541ebb70df6688
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 22:11:38 +0000

    build based on a966aef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GAM","category":"page"},{"location":"#GAM","page":"Home","title":"GAM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GAM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GAM]","category":"page"}]
+}
